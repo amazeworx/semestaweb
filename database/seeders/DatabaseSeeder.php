343ae83dd348479396ca6275f14df7e065726a11
Fransnico Susanto <fransnico@amazeworx.com>
2021-06-02 05:00:50 +0700

    Update seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,13 +6,24 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
-    }
+  /**
+   * Seed the application's database.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    $this->call([
+      CountrySeeder::class,
+      ProvinceSeeder::class,
+      CitySeeder::class,
+      //DistrictSeeder::class,
+      //SubDistrictSeeder::class,
+      OccupationSeeder::class,
+      PositionSeeder::class,
+      BusinessSeeder::class,
+      BusinessOccupationPositionSeeder::class,
+      BankSeeder::class,
+    ]);
+  }
 }
