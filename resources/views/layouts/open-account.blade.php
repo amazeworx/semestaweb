9b9ac5d27cc4ea6a31529e40cf0e22c68cdd9982
Fransnico Susanto <fransnico@amazeworx.com>
2021-06-02 02:55:18 +0700

    initial deployment

--- resources/views/layouts/open-account.blade.php
+++ resources/views/layouts/open-account.blade.php
@@ -7,6 +7,10 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
   <title>{{ config('app.name', 'Semesta Indovest') }}</title>
+  <link rel="icon" href="/images/favicon_semestaindovest-32x32.png" sizes="32x32" />
+  <link rel="icon" href="/images/favicon_semestaindovest-192x192.png" sizes="192x192" />
+  <link rel="apple-touch-icon" href="/images/appicon_semestaindovest.png" />
+  <meta name="msapplication-TileImage" content="/images/appicon_semestaindovest.png" />
 
   <!-- Fonts -->
   <link rel="preconnect" href="https://fonts.gstatic.com">
