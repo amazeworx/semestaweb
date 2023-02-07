9a688faaf68780272d698a0e80219a6c20a2d07a
Fransnico Susanto <fransnico@amazeworx.com>
2023-02-07 09:12:06 +0700

    eipo

--- resources/views/components/site-header.blade.php
+++ resources/views/components/site-header.blade.php
@@ -29,7 +29,7 @@
               </li>
               <li><a class="{{ (request()->is('download*')) ? 'active' : '' }}" href="/download">{{
                   __('header.navDownload') }}</a></li>
-              <li><a href="https://www.e-ipo.co.id/en/ipo/93/trgu-pt-cerestar-indonesia-tbk" target="_blank">{{
+              <li><a href="https://www.e-ipo.co.id/id/ipo/217/bdkr-pt-berdikari-pondasi-perkasa-tbk" target="_blank">{{
                   __('header.navEIPO') }}</a></li>
               <li><a class="{{ (request()->is('contact-us*')) ? 'active' : '' }}" href="/contact-us">{{
                   __('header.navContact') }}</a></li>
