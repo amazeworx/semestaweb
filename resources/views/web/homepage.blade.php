bc4505e8ed2591b2235b2c9d1cd81b5dc947b9be
Fransnico Susanto <62655222+amazeworx@users.noreply.github.com>
2024-12-03 15:54:05 +0700

    Update homepage.blade.php

--- resources/views/web/homepage.blade.php
+++ resources/views/web/homepage.blade.php
@@ -14,16 +14,16 @@
             <div class="w-full h-full absolute inset-0 bg-black bg-opacity-60 z-20 xl:bg-opacity-25"></div>
             <div class="flex h-full w-full items-center max-w-screen-xl mx-auto z-30 px-4 xl:px-12 pt-20 pb-4 xl:pt-36">
               <div class="relative w-full md:w-2/3 lg:w-7/12 text-white">
-                <h1 class="font-display font-bold leading-tight text-4xl md:text-5xl xl:text-6xl mb-3 md:mb-4">
+                <h1 class="font-display font-bold leading-tight text-4xl md:text-5xl xl:text-6xl mb-4 md:mb-8">
                   Kecepatan dan kemudahan dalam genggaman.
                 </h1>
                 <div class="mt-6 xl:mt-8">
-                <p class="text-base md:text-xl xl:text-2xl mb-4 xl:mb-6 font-bold">
-                  Coming soon
-                </p>
-                   <p class="text-base md:text-xl xl:text-2xl">
-                  All new Semesta Online Mobile App.
-                </p>
+                  <p class="text-base md:text-xl xl:text-2xl mb-4 xl:mb-4 font-bold">
+                    Coming soon
+                  </p>
+                  <p class="text-base md:text-xl xl:text-2xl">
+                    All new Semesta Online Mobile App.
+                  </p>
                 </div>
               </div>
             </div>
